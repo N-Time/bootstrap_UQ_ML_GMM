af93b4d2c5b50d946df8ca527a6ff118fd2b0e40
N-Time <51726922+N-Time@users.noreply.github.com>
2023-04-11 21:31:30 +0800

    Add files via upload
    1) Debug: the wrong bins determination in .error_md_std(...);
    2) Improve:
    a) .error_md_std(...): 'minsamples', and 'scale';
    b) new func: .get_sigma_df(...) for straightly read-calculate-out sigma_E,A,T.

--- mlgmmuq.py
+++ mlgmmuq.py
@@ -8,6 +8,7 @@ Functions for ML-GMMs UQ
 """
 
 import numpy as np
+import pandas as pd
 
 def func_sigma_E(y_true, y_hat_b_im, pick_cvg_b=40, nan_flag=False):
     """
@@ -40,16 +41,22 @@ def func_sigma_E(y_true, y_hat_b_im, pick_cvg_b=40, nan_flag=False):
     
     return y_tile_cvg, sigma_T, sigma_E, sigma_A
 
-def error_md_std(df, x_col, y_col, x_range, error_bins, sym=True):
+def error_md_std(df, x_col, y_col, x_range, error_bins, sym=True,
+                 minsamples=30, scale='linear'):
     """
     Calculate error bars
     """
-    x_bin_edge = np.linspace(x_range[0],x_range[1],error_bins+1)
+    if scale=='linear':
+        x_bin_edge = np.linspace(x_range[0],x_range[1],error_bins+1)
+    elif scale=='log':
+        x_bin_edge = np.logspace(np.log10(x_range[0]),
+                                 np.log10(x_range[1]),
+                                 error_bins+1)
     ymd = []
     yerr = []
     
-    x_s_list = x_bin_edge[range(0,error_bins,2)]
-    x_e_list = x_bin_edge[range(1,error_bins+1,2)]
+    x_s_list = x_bin_edge[range(0,error_bins,1)]
+    x_e_list = x_bin_edge[range(1,error_bins+1,1)]
     if error_bins % 2 == 0:  # even bins
         x_s_list = np.append(x_s_list, x_e_list[-1])
         x_e_list = np.append(x_e_list, x_bin_edge[-1])
@@ -59,16 +66,102 @@ def error_md_std(df, x_col, y_col, x_range, error_bins, sym=True):
         samples_i = df.loc[(df[x_col] >= x_s) & (df[x_col] < x_e),
                            y_col]
         
-        ymd.append(np.median(samples_i))
-        
-        if sym == True:
-            yerr.append(np.std(samples_i))
-        else:
-            if len(samples_i) == 0:
-                yerr.append([np.nan, np.nan])
-            else:
-                yerr.append([np.percentile(samples_i,14),
-                             np.percentile(samples_i,86)])
+        if samples_i.shape[0] >= minsamples:  # only for sufficient samples
+            ymd.append(np.median(samples_i))
             
+            if sym == True:
+                yerr.append(np.std(samples_i))
+            else:  # sym is a percentile tuple for (lower, upper)
+                if len(samples_i) == 0:
+                    yerr.append([np.nan, np.nan])
+                else:
+                    yerr.append([np.percentile(samples_i,sym[0]),
+                                 np.percentile(samples_i,sym[1])])
+        else:
+            ymd.append(np.nan)
+            yerr.append([np.nan, np.nan])
+            
+    ymd = np.array(ymd)
+    yerr = np.array(yerr).T - ymd
+    # yerr = abs(yerr)  # the errorbar in plt requires all values >= 0
+        
+    return x_bin, ymd, yerr
+
+def get_sigma_df(im, Tmodel='Tαmodels', pick_cvg_b=40):
+    """
+    Read the bootstrap resultos y_hat_b of a training model of IM.
+    Output the training set X and the corresponding sigma_E,A,T in pd.df
+    """
+    # choose an IM
+    # im = 'PGA (g)'
+    # Tmodel = 'Tαmodels'
+    # num_bstp = 40  # determine by the source data y_hat_b columns
     
-    return x_bin, np.array(ymd), np.array(yerr).T
+    # read from a specific folder for a T-model and im
+    y_hat_b_all = pd.read_excel('./bootstrapping/'+ Tmodel +'/' + im +
+                                '/bstp_train_yb_15k_500.xlsx',
+                                index_col=[0])
+
+    # sigma of Tα-models
+    # pick_cvg_b = 40
+    y_hat_b_im = y_hat_b_all.\
+        iloc[:, y_hat_b_all.columns.get_loc(im) + 1:].values
+    y_true = y_hat_b_all[im].values
+    y_tile_cvg, sigma_T, sigma_E, sigma_A = \
+        func_sigma_E(y_true, y_hat_b_im, pick_cvg_b)
+    
+    # Dataframe preparation
+    # X, y_true and y_hat (i.e. y_pred on train) from the source data
+    if Tmodel=='Tαmodels':
+        insert_col = 'train_pred_'+im
+    else:
+        insert_col = im
+    
+    df = y_hat_b_all.iloc[:, :y_hat_b_all.columns.
+                          get_loc(insert_col)+1]
+    # y_unbias (i.e. avg. of y_hat_b), sigma_E,A,T
+    df.insert(df.columns.get_loc(insert_col)+1,
+              "y_unbias", y_tile_cvg)
+    df.insert(df.columns.get_loc('y_unbias')+1, "sigma_E", sigma_E)
+    df.insert(df.columns.get_loc('sigma_E')+1, "sigma_A", sigma_A)
+    df.insert(df.columns.get_loc('sigma_A')+1, "sigma_T", sigma_T)
+    
+    
+    # Region as text
+    regionNum = sorted(df['Region'].unique())
+    regionLabel = ['Other','WUS','Japan','WCN','Italy','Turkey','Taiwan']
+    
+    for i, num in enumerate(regionNum):
+        df.loc[df['Region']==num,'Region'] = regionLabel[i]
+    
+    df['Region'] = df['Region'].astype('category')
+    
+    # Simplify the col. name
+    df.rename(columns={'Northern CA/Southern CA - H11 Z2.5 (m)': 'Z2.5 (m)',
+                       'Rx': 'Rx (km)',
+                       'Ry 2': 'Ry (km)',
+                       'Earthquake Magnitude': 'M',
+                       'Rake Angle (deg)': 'Rake (deg)',
+                       'Hypocenter Depth (km)': 'Dhyp (km)',
+                       'Fault Rupture Width (km)': 'W (km)',
+                       },
+              inplace=True)
+    
+    # Reorder
+    if Tmodel=='Tαmodels': 
+        order = ['M', 'Dip (deg)', 'Rake (deg)',
+                 'Dhyp (km)', 'Ztor (km)', 'W (km)',
+                 'Rjb (km)', 'Rrup (km)', 'Rx (km)', 'Ry (km)',
+                 'Vs30 (m/s)', 'Z2.5 (m)', 'Region',
+                 im, 'train_pred_'+im, 'y_unbias',
+                 'sigma_E', 'sigma_A', 'sigma_T']
+    else:
+        order = ['M', 'Dip (deg)', 'Rake (deg)',
+                 'Dhyp (km)', 'Ztor (km)', 'W (km)',
+                 'Rjb (km)', 'Rrup (km)', 'Rx (km)', 'Ry (km)',
+                 'Vs30 (m/s)', 'Z2.5 (m)', 'Region',
+                 im, 'y_unbias',
+                 'sigma_E', 'sigma_A', 'sigma_T']
+    df = df[order]
+    
+    return df
